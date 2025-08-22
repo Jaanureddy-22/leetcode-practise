386c66eae6da6e21bf2aa18fef5cb0cb9b0a820b
jaahnavi <jahnavireddybodepati@gmail.com>
2025-08-22 21:45:40 +0530

    return single num

--- single number.py
+++ single number.py
@@ -2,5 +2,5 @@ class Solution:
     def singleNumber(self, nums):
         index = 0
         for num in nums:
-            index ^= num
+            index ^= num  # EOR(^) helps to find unique numer in a list 
         return index
