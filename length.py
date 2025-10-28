18eb80440758bdf5702f64cd349a342f0d5a5fe2
jaahnavi <jahnavireddybodepati@gmail.com>
2025-10-28 06:51:27 +0530

    length of lastword

--- length.py
+++ length.py
@@ -4,8 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # using inbuilt methods
         s.strip()
         words=s.split()
         return len(words[-1])
+    
+
+
+        # without inbuilt methods
+        length = 0
+        i = len(s) - 1
+
+        # Step 1: Skip whitespaces
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        # Step 2: Count letters of last word
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+
+        return length
+
 
         
