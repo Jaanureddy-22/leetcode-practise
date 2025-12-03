5c762ddc4f5bdc49607a075bab81b926ebccf3c3
jaahnavi <jahnavireddybodepati@gmail.com>
2025-12-03 20:39:50 +0530

    searching in array

---
+++ searching in sorted array.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+        return -1
+        
+
+    #if array is already sorted then use binary search method
+
+        """left,right=0,len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid+1
+            else:
+                right=mid-1
+        return -1"""
+        
