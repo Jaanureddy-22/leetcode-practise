589cb39b33ddc606d9c4641790bde92b2a26a350
jaahnavi <jahnavireddybodepati@gmail.com>
2025-12-04 19:36:53 +0530

    romoving specific elements

--- Remove elements.py
+++ Remove elements.py
@@ -5,14 +5,10 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        nums.sort()  #noneed but make the values in ascending order
-        k=0
-        n=len(nums)  #assigning length of nums to 'n'
-        for i in range(n):
-            if nums[i]!=val:  #condition to check value of nums is given val
-                nums[k]=nums[i] #if not assign nums values to k 
-                k+=1            #increment the count of size
-        return k
+        for i in range(len(nums)):
+            if val in nums:
+                nums.remove(val)
+        return len(nums)
 
 
         
