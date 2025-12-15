8565fa2906b99ec8ea307b4b7b978cd2eb983a56
jaahnavi <jahnavireddybodepati@gmail.com>
2025-12-15 19:38:49 +0530

    Check if all A's appears before B's

---
+++ Check String.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def checkString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        string = 'ba'
+        if string in s:
+            return False
+        return True
+        
