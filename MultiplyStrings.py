19e9aee21b0ad0515cebf22a8bdb57b61b778cbc
jaahnavi <jahnavireddybodepati@gmail.com>
2026-02-21 22:07:43 +0530

    multiplying strings

---
+++ MultiplyStrings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        nums=int(num1)*int(num2)
+        return str(nums)
+        
